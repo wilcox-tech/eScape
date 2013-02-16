d5d4a42fb2b82e4197ca494e420e7c356f1bcf64
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2013-02-16 07:42:03 -0500

    Use xcode-select to determine /Developer path

--- build/iPhone/build-for-iPhone.sh
+++ build/iPhone/build-for-iPhone.sh
@@ -10,14 +10,16 @@ echo
 make clean || echo "Okay, we're starting fresh"
 echo 
 
+XCODE_PATH=$(xcode-select --print-path)
+
 
 # Create simulator binaries
 cmake -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
--DCMAKE_C_FLAGS:STRING="-miphoneos-version-min=3.0 -isysroot /Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator5.0.sdk" \
--DCMAKE_CXX_FLAGS:STRING='-miphoneos-version-min=3.0 -isysroot /Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator5.0.sdk' \
+-DCMAKE_C_FLAGS:STRING="-miphoneos-version-min=3.0 -isysroot $XCODE_PATH/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator5.1.sdk" \
+-DCMAKE_CXX_FLAGS:STRING='-miphoneos-version-min=3.0 -isysroot $XCODE_PATH/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator5.1.sdk' \
 -DCMAKE_OSX_ARCHITECTURES:STRING=i386 \
--DCMAKE_OSX_SYSROOT:PATH=/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator5.0.sdk \
--DCMAKE_SHARED_LINKER_FLAGS:STRING='-isysroot /Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator5.0.sdk' \
+-DCMAKE_OSX_SYSROOT:PATH=$XCODE_PATH/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator5.1.sdk \
+-DCMAKE_SHARED_LINKER_FLAGS:STRING='-isysroot $XCODE_PATH/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator5.1.sdk' \
 -DEXTRA_UA="$EXTRA_UA" \
 -DTINY_ESCAPE:BOOL=ON \
 -DBUILD_IPHONE:BOOL=ON \
@@ -35,11 +37,11 @@ rm CMakeCache.txt
 
 # Create iPhone binaries
 cmake -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
--DCMAKE_C_FLAGS:STRING="-mthumb -miphoneos-version-min=3.0 -isysroot /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.0.sdk" \
--DCMAKE_CXX_FLAGS:STRING='-mthumb -miphoneos-version-min=3.0 -isysroot /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.0.sdk' \
+-DCMAKE_C_FLAGS:STRING="-mthumb -miphoneos-version-min=3.0 -isysroot $XCODE_PATH/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.1.sdk" \
+-DCMAKE_CXX_FLAGS:STRING='-mthumb -miphoneos-version-min=3.0 -isysroot $XCODE_PATH/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.1.sdk' \
 -DCMAKE_OSX_ARCHITECTURES:STRING="armv6;armv7" \
--DCMAKE_OSX_SYSROOT:PATH=/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.0.sdk \
--DCMAKE_SHARED_LINKER_FLAGS:STRING='-isysroot /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.0.sdk' \
+-DCMAKE_OSX_SYSROOT:PATH=$XCODE_PATH/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.1.sdk \
+-DCMAKE_SHARED_LINKER_FLAGS:STRING='-isysroot $XCODE_PATH/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.1.sdk' \
 -DEXTRA_UA="$EXTRA_UA" \
 -DBUILD_IPHONE:BOOL=ON \
 -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/clang++ -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/clang \
